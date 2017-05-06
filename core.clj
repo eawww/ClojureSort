a23a0d463b4cf9200b0a81d44b2b3f89b4bee547
Eric Wilson <eawww0@gmail.com>
2017-05-06 00:24:39 -0500

    Add comments, sources, execution calls

--- core.clj
+++ core.clj
@@ -1,7 +1,9 @@
 (ns thatclojureproject.core (:require [clojure.java.io :as io]))
 
-(def filename "numbers.txt") ;const defines filename for easy changin'
+(def filename "numbers_txt.txt") ;const defines filename for easy changin'
 
+;loosely based on
+;http://stackoverflow.com/questions/9047231/read-a-file-into-a-list-each-element-represents-one-line-of-the-file
 (defn getlistfromfile []
   (with-open [readr (io/reader filename)]
     (doall (map read-string (line-seq readr)));jams all the values as ints into list
@@ -10,11 +12,12 @@
 
 (def numberlst (getlistfromfile)) ;for some dumb reason, we have to do it this way to treat the return of the function as a collection
 
+
 (defn merg
   "merges two sorted lists into one sorted list"
   [leftlst rightlst]
   (loop [lf leftlst rt rightlst result [] ]
-    (cond
+    (cond ;build merged list out of first elements of each
       (empty? lf)                 (concat result rt)
       (empty? rt)                 (concat result lf)
       (> (first lf) (first rt))   (recur lf (rest rt) (conj result (first rt)))
@@ -31,24 +34,29 @@
          (apply merg 
           (map mergsort 
            (split-at (quot (count numbers) 2) numbers)
-           )
-          )
+           ;split at because partition can drop elements off the end
+           ;quot because split-at will return empty if given a fraction index
+           );divide list in half and apply merge algorithm to sorted
+          );results of each side
    )
 )
 
+;referenced idea from answer on
+;http://stackoverflow.com/questions/36731188/multithreaded-merge-sort-algorithm-in-clojure
+;but made it my own and compacted it into a single function
 (defn threadulesque-mergsort 
   "Lets you specify number of threads (please only powers of 2)"
   [numbers threads]
   (if (< (count numbers) 2) 
-    numbers
-    (if (> threads 2)
+    numbers ;just in case we're already at size 1 or 0
+    (if (> threads 2) ;see if we've made enough threads
 	    (apply merg
 	           (pmap threadulesque-mergsort
 	                 (split-at (quot (count numbers) 2) numbers)
-	                 [(quot threads 2) (quot threads 2)]
-	           )
-	    )
-	    (apply merg
+	                 [(quot threads 2) (quot threads 2)] ;divide threads by 2
+            )                                          ;on each recurse
+	    ) ;if it's the last time, we don't need to recur into this function
+	    (apply merg ;but, rather the basic merge sort
 	           (pmap mergsort
 	                 (split-at (quot (count numbers) 2) numbers)
 	            )
@@ -57,3 +65,16 @@
   )
 )
 
+(println "1 thread")
+(time (mergsort numberlst))
+(println "2 threads")
+(time (threadulesque-mergsort numberlst 2))
+(println "4 threads")
+(time (threadulesque-mergsort numberlst 4))
+(println "8 threads")
+(time (threadulesque-mergsort numberlst 8))
+(println "16 threads")
+(time (threadulesque-mergsort numberlst 16))
+(println "32 threads")
+(time (threadulesque-mergsort numberlst 32))
+      
